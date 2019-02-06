ce23d178f554108d3405732221e722671893b81d
Gabriel Caiana Guedes <gabrielcaianaguedes@gmail.com>
2019-02-05 22:52:45 -0200

    criando if e loop while com calculo if

--- js/app.js
+++ js/app.js
@@ -2,3 +2,10 @@
   let operador = 'adicao'; // Tipo de cálculo (o padrão é adição)
   let i = 1; // Configura o contador como 1
   let mensagem = ''; // Mensagem
+
+  if (operador === 'adicao') { // Se a variavel operador for 'adicao'
+    while(i < 11) { // Enquanto o contador for menor que 11
+      mensagem += `${i} + ${tabuada} = ${i + tabuada} <br />`; // Calculo
+      i++; // Soma 1 ao contador
+    }
+  }
