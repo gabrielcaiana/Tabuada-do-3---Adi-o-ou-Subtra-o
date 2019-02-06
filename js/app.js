773d2fdcad5929c537ff19a147bb883f9764979f
Gabriel Caiana Guedes <gabrielcaianaguedes@gmail.com>
2019-02-05 22:49:43 -0200

    criando 4 variaveis

---
+++ js/app.js
@@ -0,0 +1,4 @@
+  let tabuada = 3; // Unidade da Tabuada
+  let operador = 'adicao'; // Tipo de cálculo (o padrão é adição)
+  let i = 1; // Configura o contador como 1
+  let mensagem = ''; // Mensagem
